98413d794b0efcbab16f84ccc5c162f86b348c70
sksahoo-techrayslabs <sksahoo@techrayslabs.com>
2026-02-07 20:08:52 +0530

    approx ui done

--- dist/typescript.js
+++ dist/typescript.js
@@ -1,7 +1,3 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function greet(person) {
-    return "hello" + person;
-}
-console.log(greet("sobhan"));
+
+// export {};
 //# sourceMappingURL=typescript.js.map
--- src/typescript.ts
+++
@@ -1,5 +0,0 @@
-function greet(person: string):string{
-    return "hello"+person;
-}
-
-console.log(greet("sobhan"));
