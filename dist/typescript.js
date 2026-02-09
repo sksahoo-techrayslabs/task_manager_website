63d7761a36a3509276e8a119ca9e4fd96fcae65f
sksahoo-techrayslabs <sksahoo@techrayslabs.com>
2026-02-09 16:58:03 +0530

    ts file added and buttons and evnts are wroking ,ts is converted to js wit command tsc

--- dist/typescript.js
+++ dist/typescript.js
@@ -4,3 +4,70 @@ console.log("task manager is working");
 var form = document.getElementById("taskinput");
 var tasklist = document.getElementById("task-list");
 var template = document.getElementById("task-template");
+// ---------------------storing the initial data from html to local storage
+var tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+// showing the tasks when my page reloads 
+showtasks();
+// -------------------------now the event listner works and adds ther form
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    var task = {
+        name: document.getElementById("name").value,
+        subject: document.getElementById("subject").value,
+        date: document.getElementById("date").value,
+        message: document.getElementById("message").value,
+        status: "pending",
+    };
+    tasks.push(task);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    showtasks();
+    form.reset();
+});
+// ---------------- SHOW TASKS ----------------
+function showtasks() {
+    tasklist.innerHTML = "";
+    tasks.forEach(function (task, index) {
+        var clone = template.content.cloneNode(true); // this will copy all the elements to clone variable since my template section in html is hidden i.e it will not show in ui
+        var card = clone.firstElementChild;
+        // ----------filling the tasks data--------------- card refers to as one single task
+        card.querySelector(".task-subject").textContent = task.subject;
+        card.querySelector(".task-name").textContent = task.name;
+        card.querySelector(".task-date").textContent = task.date;
+        card.querySelector(".task-message").textContent = task.message;
+        card.querySelector(".task-status").textContent = task.status;
+        //------------button functions----it fetches the action button from cards
+        var completebtn = card.querySelector(".complete-btn");
+        var editbtn = card.querySelector(".edit-btn");
+        var deletebtn = card.querySelector(".delete-btn");
+        completebtn.onclick = function () {
+            if (tasks[index].status === "pending") {
+                tasks[index].status = "completed";
+            }
+            else {
+                tasks[index].status = "pending";
+            }
+            localStorage.setItem("tasks", JSON.stringify(tasks));
+            showtasks();
+        };
+        editbtn.onclick = function () {
+            var newmsg = prompt("Edit task", task.message);
+            if (newmsg != null) {
+                tasks[index].message = newmsg;
+                localStorage.setItem("tasks", JSON.stringify(tasks));
+                showtasks();
+            }
+        };
+        deletebtn.onclick = function () {
+            tasks.splice(index, 1);
+            localStorage.setItem("tasks", JSON.stringify(tasks));
+            showtasks();
+        };
+        if (task.status === "completed") {
+            completebtn.textContent = "MArk as pending";
+        }
+        else {
+            completebtn.textContent = "Mark as completed";
+        }
+        tasklist.appendChild(clone);
+    });
+}
--- src/typescript.ts
+++ src/typescript.ts
@@ -16,3 +16,95 @@ type Task =
     status: "pending" | "completed";
 
   };
+// ---------------------storing the initial data from html to local storage
+
+let tasks: Task[] = JSON.parse(localStorage.getItem("tasks") || "[]");
+
+// showing the tasks when my page reloads 
+showtasks();
+
+// -------------------------now the event listner works and adds ther form
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const task: Task =
+  {
+    name: (document.getElementById("name") as HTMLInputElement).value,
+    subject: (document.getElementById("subject") as HTMLInputElement).value,
+    date: (document.getElementById("date") as HTMLInputElement).value,
+    message: (document.getElementById("message") as HTMLTextAreaElement).value,
+    status: "pending",
+  };
+
+
+  tasks.push(task);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  showtasks();
+  form.reset();
+
+});
+
+// ---------------- SHOW TASKS ----------------
+
+function showtasks() {
+  tasklist.innerHTML = "";
+
+  tasks.forEach((task, index) => {
+    const clone = template.content.cloneNode(true) as HTMLElement; // this will copy all the elements to clone variable since my template section in html is hidden i.e it will not show in ui
+    const card = clone.firstElementChild as HTMLDivElement;
+
+
+    // ----------filling the tasks data--------------- card refers to as one single task
+    card.querySelector(".task-subject")!.textContent = task.subject;
+    card.querySelector(".task-name")!.textContent = task.name;
+    card.querySelector(".task-date")!.textContent = task.date;
+    card.querySelector(".task-message")!.textContent = task.message;
+    card.querySelector(".task-status")!.textContent = task.status;
+
+
+
+
+    //------------button functions----it fetches the action button from cards
+    const completebtn = card.querySelector(".complete-btn") as HTMLButtonElement;
+    const editbtn = card.querySelector(".edit-btn") as HTMLButtonElement;
+    const deletebtn = card.querySelector(".delete-btn") as HTMLButtonElement;
+
+
+    completebtn.onclick = function () {
+  if (tasks[index]!.status === "pending") {
+    tasks[index]!.status = "completed";
+  } else {
+    tasks[index]!.status = "pending";
+  }
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  showtasks();
+};
+
+    editbtn.onclick = function () {
+      const newmsg = prompt("Edit task", task.message);
+      if (newmsg != null) {
+        tasks[index]!.message = newmsg;
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+        showtasks();
+      }
+    };
+
+    deletebtn.onclick = function () {
+      tasks.splice(index, 1);
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+      showtasks();
+    };
+
+    if (task.status === "completed") {
+      completebtn.textContent="MArk as pending";
+    }
+    else{
+      completebtn.textContent="Mark as completed"
+    }
+
+    tasklist.appendChild(clone);
+
+  });
+}
